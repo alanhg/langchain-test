0fc1c8f2cf7629324a9faf4c0b72c39ccba04108
alanhe <qianghe421@gmail.com>
2023-05-14 20:24:05 +0800

    prompt支持定制化

--- privateGPT.py
+++ privateGPT.py
@@ -2,18 +2,36 @@ from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from constants import CHROMA_SETTINGS
-from langchain import OpenAI
+from langchain import OpenAI, PromptTemplate
 
 embeddings = HuggingFaceEmbeddings(model_name="GanymedeNil/text2vec-large-chinese")
+
+
 # embeddings = OpenAIEmbeddings()
 
 def main():
+    prompt_template = """基于以下已知信息，简洁和专业的来回答用户的问题。
+    如果无法从中得到答案，请说 "根据已知信息无法回答该问题" 或 "没有提供足够的相关信息"，不允许在答案中添加编造成分，答案请使用中文。
+    已知内容:
+    {context}
+    问题:
+    {question}"""
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+    chain_type_kwargs = {"prompt": prompt}
+
     # 加载持久化DB数据
     db = Chroma(persist_directory="./db", embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever()
 
     qa = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0), chain_type="stuff", retriever=retriever,
+                                     chain_type_kwargs=chain_type_kwargs,
                                      return_source_documents=False)
+    qa.combine_documents_chain.document_prompt = PromptTemplate(
+        input_variables=["page_content"], template="{page_content}"
+    )
 
     # Interactive questions and answers
     while True:
@@ -35,6 +53,7 @@ def main():
         for document in docs:
             print("\n> " + document.metadata["source"] + ":")
             print(document.page_content)
+
 
 if __name__ == "__main__":
     main()
